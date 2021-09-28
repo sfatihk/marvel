564f3515128990a94b2b2ea4f031da0ddbb1db50
Seyyid Fatih KOÇ <sakkarmen@gmail.com>
2021-09-29 00:53:07 +0300

    init

---
+++ src/App.test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import App from "./App";
+
+test("empty case", () => {});
