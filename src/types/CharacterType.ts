57be88341042866722961e9d7f787c24e2c5f097
Seyyid Fatih KOÇ <sakkarmen@gmail.com>
2021-09-29 19:47:45 +0300

    Integrate service and store (#3)
    * Create ComicType.ts
    
    * Create ThumnailType.ts
    
    * Create CharacterType.ts
    
    * Create fetch character api request
    
    * Create character api service
    
    * Create character reducer
    
    * Update store reducers for character reducer
    
    * Add demo usage

---
+++ src/types/CharacterType.ts
@@ -0,0 +1,10 @@
+import { ThumnailType } from "./ThumnailType";
+import { ComicType } from "./ComicType";
+
+export type CharacterType = {
+  id: number;
+  name: string;
+  thumbnail: ThumnailType;
+  description: string;
+  comics: ComicType[];
+};
