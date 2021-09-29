430d476fec8bb80ca373c1d13cd78c0a448d05ba
Seyyid Fatih KOÇ <sakkarmen@gmail.com>
2021-09-29 11:10:18 +0300

    Update folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./App/App";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
