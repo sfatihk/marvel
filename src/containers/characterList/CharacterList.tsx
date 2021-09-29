a1ce06312d28e5a90ba2b1bfda296808982dd0b0
Seyyid Fatih KOÇ <sakkarmen@gmail.com>
2021-09-29 23:34:01 +0300

    Create CharacterList.tsx

---
+++ src/containers/characterList/CharacterList.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import { Col, Row } from "antd";
+
+//store
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import {
+  getCharacterList,
+  getCharacters,
+} from "../../store/features/characterSlice";
+import Character from "../../components/Character";
+
+const CharacterList = () => {
+  const dispatch = useAppDispatch();
+  const { status, results, count } = useAppSelector(getCharacterList);
+
+  useEffect(() => {
+    if (count === 0) {
+      dispatch(getCharacters(0));
+    }
+  }, [count, dispatch]);
+
+  return (
+    <Row>
+      {results &&
+        results.map((character) => (
+          <Character key={character.id} {...character} />
+        ))}
+      {status !== "loading" && (
+        <Col xs={{ span: 23, offset: 1 }} lg={{ span: 22, offset: 2 }}>
+          <button onClick={() => dispatch(getCharacters(count))}>
+            Load More
+          </button>
+        </Col>
+      )}
+    </Row>
+  );
+};
+
+export default CharacterList;
