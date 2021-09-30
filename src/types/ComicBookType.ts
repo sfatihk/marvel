152c487dcb979b1f49cf7db0df7cb7ea136a3162
Seyyid Fatih KOÇ <sakkarmen@gmail.com>
2021-09-30 12:55:43 +0300

    Update ComicBookType.ts

--- src/types/ComicBookType.ts
+++ src/types/ComicBookType.ts
@@ -1,3 +1,27 @@
+import { ThumnailType } from "./ThumnailType";
+
+type UrlType = {
+  type: string;
+  url: string;
+};
+type DateType = {
+  type: "onsaleDate" | "focDate" | "unlimitedDate" | "digitalPurchaseDate";
+  date: string;
+};
+type PriceType = {
+  type: "printPrice" | "digitalPurchasePrice";
+  price: number;
+};
 export type ComicBookType = {
   name: string;
+};
+
+export type ExtendedComicBookType = {
+  id: number;
+  title: string;
+  pageCount: number;
+  urls: UrlType[];
+  dates: DateType[];
+  prices: PriceType[];
+  thumbnail: ThumnailType;
 };
